26c9b0a3fc61a21f504ab722f14d9376c9e64ee1
jew2222 <72792459+jew2222@users.noreply.github.com>
2024-02-14 00:00:04 +0900

    Feat : 카카오로그인 버튼

--- src/pages/login-page/index.jsx
+++ src/pages/login-page/index.jsx
@@ -1,22 +1,32 @@
 import { useEffect, useState } from 'react'
 import LoginPageView from './login-page'
 
-const LoginPage =() => {
+const LoginPage = () => {
+  const Rest_api_key = import.meta.env.VITE_KAKAO_KEY
+  const redirect_uri = 'http://localhost:5173/'
+  // oauth 요청 URL
+  const kakaoURL = `https://kauth.kakao.com/oauth/authorize?client_id=${Rest_api_key}&redirect_uri=${redirect_uri}&response_type=code`
+  const handleKakaoLogin = () => {
+    window.location.href = kakaoURL
+  }
+
   const [id, setId] = useState("")
   const [psw, setPsw] = useState("")
 
-  const onChangeId = (event) =>{
+  const onChangeId = (event) => {
     setId(event.target.value)
   }
-  const onChangePwd = (event) =>{
+  const onChangePwd = (event) => {
     setPsw(event.target.value)
   }
-  const handleLogin = ()=>{
-      return
+  const handleLogin = () => {
+    return
   }
 
+
+
   return (
-  <LoginPageView id={id} psw={psw} onChangeId={onChangeId} onChangePwd={onChangePwd} handleLogin={handleLogin}/>
+    <LoginPageView id={id} psw={psw} onChangeId={onChangeId} onChangePwd={onChangePwd} handleLogin={handleLogin} handleKakaoLogin={handleKakaoLogin} />
   )
 }
 
--- src/pages/login-page/login-page.jsx
+++ src/pages/login-page/login-page.jsx
@@ -1,20 +1,21 @@
-const LoginPageView =({id, psw, onChangeId, onChangePwd,handleLogin})=>{ //프롭스 대신 객체로 넣기(타입)
-  return(
+const LoginPageView = ({ id, psw, onChangeId, onChangePwd, handleLogin, handleKakaoLogin }) => { //프롭스 대신 객체로 넣기(타입)
+  return (
     <div>
-    <h1>로그인</h1>
-    <input type="text"
-      onChange={onChangeId}
-      value={id}
-      placeholder='아이디' />
-    <input type="password" 
-       onChange={onChangePwd}
-       value={psw}
-       placeholder='비번'/>
+      <h1>로그인</h1>
+      <input type="text"
+        onChange={onChangeId}
+        value={id}
+        placeholder='아이디' />
+      <input type="password"
+        onChange={onChangePwd}
+        value={psw}
+        placeholder='비번' />
 
-    <button onClick={handleLogin}>로그인</button>
+      <button onClick={handleLogin}>로그인</button>
+      <button onClick={handleKakaoLogin}>카카오 로그인</button>
 
-  </div>
-)
+    </div>
+  )
 };
 
 export default LoginPageView;
