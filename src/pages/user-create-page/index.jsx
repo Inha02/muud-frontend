a0075d4b1c48714c29a50a1b84623ea4555856db
jew2222 <72792459+jew2222@users.noreply.github.com>
2024-02-14 23:59:13 +0900

    Add : 유저 닉네임 생성 페이지 추가

---
+++ src/pages/user-create-page/index.jsx
@@ -0,0 +1,11 @@
+import { useEffect, useState } from 'react'
+import UserCreatePageView from './user-create-page'
+
+const UserCreatePage = () => {
+
+  return (
+    <UserCreatePageView />
+  )
+}
+
+export default UserCreatePage
---
+++ src/pages/user-create-page/user-create-page.jsx
@@ -0,0 +1,10 @@
+const UserCreatePageView = () => { //프롭스 대신 객체로 넣기(타입)
+  return (
+    <div>
+      <h1>닉네임을 입력하세요</h1>
+
+    </div>
+  )
+};
+
+export default UserCreatePageView;
