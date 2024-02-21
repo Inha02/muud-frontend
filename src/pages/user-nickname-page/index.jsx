25ea2630319d0e5644bc420d7504aba9656c2403
jew2222 <72792459+jew2222@users.noreply.github.com>
2024-02-21 22:19:17 +0900

    Feat : 닉네임 입력 화면 기능

--- src/pages/user-nickname-page/index.jsx
+++ src/pages/user-nickname-page/index.jsx
@@ -2,8 +2,30 @@ import { useEffect, useState } from 'react'
 import UserNicknamePageView from './user-nickname-page'
 
 const UserNicknamePage = () => {
+  const [nickname, setNickname] = useState('')
+  const [isBtnActive, setIsBtnActive] = useState(false)
 
-  return <UserNicknamePageView />
+  const onChangeNickname = (event) => {
+    setNickname(event.target.value)
+    if (event.target.value !== '') {
+      setIsBtnActive(true)
+    } else {
+      setIsBtnActive(false)
+    }
+  }
+
+  const handleClick = () => {
+
+  }
+
+  return (
+    <UserNicknamePageView
+      nickname={nickname}
+      onChangeNickname={onChangeNickname}
+      isBtnActive={isBtnActive}
+      handleClick={handleClick}
+    />
+  )
 }
 
 export default UserNicknamePage
--- src/pages/user-nickname-page/user-nickname-page.jsx
+++ src/pages/user-nickname-page/user-nickname-page.jsx
@@ -1,13 +1,28 @@
 
-const UserNicknamePageView = () => {
+import styles from './user-nickname-page.module.css';
+import RoundButton from '../../components/common/RoundButton';
+
+const UserNicknamePageView = ({ nickname, onChangeNickname, isBtnActive, handleClick }) => {
 
   return (
-    <div>
-      <div
-        className="container"
-      >
+    <div className='appContainer back'>
+      <div>뮤디에서 사용할<br />
+        이름을 입력해주세요.
       </div>
-    </div>
+      <div>
+        이름은 공백없이 10자 이하만
+        가능합니다.
+      </div>
+      <input type="text"
+        onChange={onChangeNickname}
+        value={nickname}
+        placeholder='이름을 입력해주세요'
+        required
+        maxLength={10}
+      />
+      <RoundButton onClick={handleClick} active={isBtnActive}>완료</RoundButton>
+
+    </div >
   )
 }
 
