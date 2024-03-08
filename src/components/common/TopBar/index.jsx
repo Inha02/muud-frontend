6cb0bd0648782f52bd277d218b45e6a8dc128c86
jew2222 <jewallk@gmail.com>
2024-03-09 04:21:19 +0900

    Update: 뒤로가기 버튼 동작 수정

--- src/components/common/TopBar/index.jsx
+++ src/components/common/TopBar/index.jsx
@@ -1,20 +1,28 @@
-import { useEffect, useState } from 'react'
+import { useEffect } from 'react'
 import { useNavigate } from 'react-router-dom';
 import TopBarView from './TopBar'
+import { useUserContext } from '../../../context/UserContext';
 
 const TopBar = ({ option }) => {
   const navigateTo = useNavigate();
+  const { setIsAuthenticated } = useUserContext()
 
   const handleClick = () => {
     switch (option) {
       case 'back':
         navigateTo(-1);
         break;
+      case '/login':
+        setIsAuthenticated(false)
+        break;
       default:
-        navigateTo(-1);
+        navigateTo(option, { replace: true });
         break;
     }
   };
+
+  useEffect(() => {
+  }, [])
 
   return <TopBarView option={option} handleClick={handleClick} />
 }
--- src/layout/BackBtnContainer/index.jsx
+++ src/layout/BackBtnContainer/index.jsx
@@ -1,11 +1,22 @@
-import { Outlet } from 'react-router-dom'
+import { Outlet, useLocation } from 'react-router-dom'
 import TopBar from '../../components/common/TopBar';
 import styles from './BackBtnContainer.module.css'
 
+const getBackPath = (path) => {
+    switch (path) {
+        case "/home":
+            return "/login"
+        case "/diary/detail":
+            return "/home";
+        default:
+            return "back"
+    }
+}
 const BackBtnContainer = () => {
+    const { pathname } = useLocation();
     return (
         <div className={styles.backBtnContainer}>
-            <TopBar option={'back'} ></TopBar>
+            <TopBar option={getBackPath(pathname)} ></TopBar>
             <Outlet></Outlet>
         </div>
     );
